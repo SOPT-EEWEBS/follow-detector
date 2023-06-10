cf2d9d0bd65a0da58657976f0d90347c1818a643
Dev-jwJeong <rgn95652@gmail.com>
2023-06-10 15:30:15 +0900

    style : MainLayout 컴포넌트

---
+++ Layout/MainLayout.tsx
@@ -0,0 +1,20 @@
+// 헤더를 제외한 깃허브 맞팔 컴포넌트를 감싸는 메인 레이아웃
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return <StWrapper>{children}</StWrapper>;
+};
+
+export default MainLayout;
+
+const StWrapper = styled.main`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+`;
