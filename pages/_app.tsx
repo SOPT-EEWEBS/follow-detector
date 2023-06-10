04ae587e5d7071c9c079a31491e39bdf47a310d2
Dev-jwJeong <rgn95652@gmail.com>
2023-06-10 22:19:10 +0900

    feat : 전역 테마 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globalStyles';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
