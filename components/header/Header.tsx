c3b649b3c2269ebc307d817bb21a39b7561e7ce3
Dev-jwJeong <rgn95652@gmail.com>
2023-06-10 22:20:11 +0900

    style : 헤더 퍼블리싱

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>헤더입니다</div>;
+  return <StWrapper>깃허브 맞팔 탐지기</StWrapper>;
 };
 
 export default Header;
+
+const StWrapper = styled.header`
+  width: 100%;
+  padding: 1rem;
+
+  background-color: ${({ theme }) => theme.colors.green};
+`;
