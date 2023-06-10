ae8666f45bbb2fc46269f8c0f47da1afcb503157
Dev-jwJeong <rgn95652@gmail.com>
2023-06-10 15:19:38 +0900

    style : 전역 스타일 타입 지정

---
+++ styles/style.d.ts
@@ -0,0 +1,23 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      white: string;
+      lightPink: string;
+      lightPurple: string;
+      purple: string;
+      lightYellow: string;
+      yellow: string;
+      darkPink: string;
+      pink: string;
+      mint: string;
+      salmon: string;
+      lightGreen: string;
+      green: string;
+      darkGreen: string;
+      lightPink_modal: string;
+      darkPink_modal: string;
+    };
+  }
+}
