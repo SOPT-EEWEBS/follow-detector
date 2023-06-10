7c922bda62fdbbb25b78ee0cd8a54b8dc000df51
Dev-jwJeong <rgn95652@gmail.com>
2023-06-11 00:38:02 +0900

    feat : 루트페이지에서 유저 정보, 팔로우, 팔로워 api 호출

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,10 +1,36 @@
 // 홈 컴포넌트 /home
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/header/Header';
 import MainLayout from '../../Layout/MainLayout';
 import Profile from '../../components/profile/Profile';
+import { useRecoilState } from 'recoil';
+import { profileState } from '../../recoil/profile';
+import { getProfile } from '../../api/get/getProfile';
+import { getFollowers, getFollowing } from '../../api/get/getFollowList';
 
 const Home = () => {
+  const [userProfile, setUserProfile] = useRecoilState(profileState);
+
+  const handleGetProfile = async () => {
+    const response = await getProfile();
+    if (response) setUserProfile(response);
+  };
+
+  const handleGetFollowers = async () => {
+    const response = await getFollowers();
+    console.log(response);
+  };
+  const handleGetFollowing = async () => {
+    const response = await getFollowing();
+    console.log(response);
+  };
+
+  useEffect(() => {
+    handleGetProfile();
+    handleGetFollowers();
+    handleGetFollowing();
+  }, []);
+
   return (
     <div>
       <Header />
